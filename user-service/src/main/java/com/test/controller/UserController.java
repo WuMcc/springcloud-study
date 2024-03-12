f289d4e080d4cdfc868a419415e5cf88794979a0
WuMcc <104179156+WuMcc@users.noreply.github.com>
2024-03-12 21:34:30 +0800

    Add files via upload

--- user-service/src/main/java/com/test/controller/UserController.java
+++ user-service/src/main/java/com/test/controller/UserController.java
@@ -3,11 +3,14 @@ package com.test.controller;
 import com.test.entity.User;
 import com.test.service.UserService;
 import javax.annotation.Resource;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope//实时刷新，远端nacos服务器配置文件发生更新，实时更新
 public class UserController {
     @Resource
     UserService userService;
